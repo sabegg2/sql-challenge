d8103648255099659dcde4e0092555c88ddaf380
sabegg2 <sabegg@gmail.com>
2024-08-19 21:52:15 -0600

    created composite keys

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -6,14 +6,15 @@
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 INNER JOIN salaries s
-ON e.emp_no = s.emp_no;
+ON e.emp_no = s.emp_no
+ORDER BY e.emp_no;
 
 --2. List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date 
 FROM employees
 --WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986' -- inclusion of end points unclear 
 WHERE (hire_date >= DATE '1/1/1986' AND hire_date <= DATE '12/31/1986')
-ORDER BY hire_date;
+ORDER BY hire_date, last_name;
 
 --3. List the manager of each department along with their department number, department name, 
 --   employee number, last name, and first name.
@@ -22,7 +23,8 @@ FROM dept_manager dm
 INNER JOIN departments d
 ON dm.dept_no = d.dept_no
 INNER JOIN employees e
-ON dm.emp_no = e.emp_no;
+ON dm.emp_no = e.emp_no
+ORDER BY dm.dept_no, e.emp_no;
 
 --4. List the department number for each employee along with that employee’s employee number, 
 --   last name, first name, and department name.
@@ -31,14 +33,16 @@ FROM dept_emp de
 INNER JOIN employees e
 ON de.emp_no = e.emp_no
 INNER JOIN departments d
-ON de.dept_no = d.dept_no;
+ON de.dept_no = d.dept_no
+ORDER BY de.dept_no, e.emp_no;
 
 --5. List first name, last name, and sex of each employee whose first name is Hercules 
 --   and whose last name begins with the letter B.
 SELECT e.first_name, e.last_name, e.sex
 FROM employees e
 WHERE first_name = 'Hercules'
-AND last_name LIKE 'B%';
+AND last_name LIKE 'B%'
+ORDER BY last_name;
 
 --6. List each employee in the Sales department, including their employee number, 
 --   last name, and first name.
@@ -48,7 +52,8 @@ INNER JOIN employees e
 ON de.emp_no = e.emp_no
 INNER JOIN departments d
 ON de.dept_no = d.dept_no
-WHERE d.dept_name = 'Sales';
+WHERE d.dept_name = 'Sales'
+ORDER BY de.emp_no;
 
 --7. List each employee in the Sales and Development departments, including their employee number, 
 --   last name, first name, and department name.
@@ -59,7 +64,8 @@ ON de.emp_no = e.emp_no
 INNER JOIN departments d
 ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales' 
-OR d.dept_name = 'Development';
+OR d.dept_name = 'Development'
+ORDER BY de.emp_no;
 
 --8. List the frequency counts, in descending order, of all the employee last names 
 --   (that is, how many employees share each last name).
--- EmployeeSQL/Data_Engineering.sql
+++ EmployeeSQL/Data_Engineering.sql
@@ -16,12 +16,8 @@
 --DROP TABLE IF EXISTS departments;
 --DROP TABLE IF EXISTS titles;
 
--- The following code is developed off of an export from QuickDBD after creating 
---   DB schema diagram: https://www.quickdatabasediagrams.com/.
--- Variable types modified to VARCHAR(#) instead of strings.
--- PRIMARY KEYs identified explicitly rather as CONSTRAINTs.
 -- Note that PRIMARY KEYs are automatically set to NOTNULL by default.
--- PRIMARY KEYs identified CREATE TABLE rather than via ALTER TABLE.
+-- PRIMARY KEYs AND FOREIGN KEYs identified CREATE TABLE rather than via ALTER TABLE.
 
 CREATE TABLE titles (
     title_id CHAR(5) PRIMARY KEY,
@@ -47,37 +43,22 @@ CREATE TABLE employees (
 CREATE TABLE salaries (
     emp_no int NOT NULL,
     salary money NOT NULL,
-	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY(emp_no)
 );
 
 CREATE TABLE dept_emp (
     emp_no int NOT NULL,
     dept_no CHAR(4) NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY(emp_no,dept_no) -- composite key, since have some empolyees in multiple departments
 );
 
 CREATE TABLE dept_manager (
     dept_no CHAR(4) NOT NULL,
     emp_no int NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY(emp_no,dept_no) -- composite key, since possible to have one employee a manager for more than one department
 );
-
---ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
---REFERENCES titles (title_id);
-
---ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
---REFERENCES employees (emp_no);
-
---ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
---REFERENCES employees (emp_no);
-
---ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
---REFERENCES employees (emp_no);
-
---ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
---REFERENCES departments (dept_no);
-
---ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
---REFERENCES departments (dept_no);
